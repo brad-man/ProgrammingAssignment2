8838a1d678a77c25a3b66671ba0f8ad1d28d551f
Brad Weaver <Bradley.Weaver@fortworthtexas.gov>
2015-06-19 10:38:22 -0500

    Final product (hopefully)

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Allows user to cache matrix and/or inverse
+## and then retrieve or calculate inverse of matrix on command.
 
-## Write a short comment describing this function
+## Returns a list of functions designed to cache/retrieve a matrix or its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
       inv <- NULL
@@ -19,8 +19,18 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## Checks to see if inverse is in cache, if not calculates
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+      invmtx <- x$getinv()
+      if (is.null(invmtx)) {
+            print ('calculating inverse...')
+            mtx <- x$get()
+            return (solve(mtx))
+      }
+      else {
+            print('returning cached data')
+            return(invmtx)
+      }
 }
